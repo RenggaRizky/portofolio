f8407f41769c29376e9c11a9abb5c19ecd1b40ed
RenggaRizky <renggarizkys@gmail.com>
2021-07-05 22:54:03 +0700

    make layout for tablet

---
+++ js/link-image.js
@@ -0,0 +1,11 @@
+const project1 = document.querySelector(".project-1");
+const project2 = document.querySelector(".project-2");
+const project3 = document.querySelector(".project-3");
+
+project2.addEventListener("click", () => {
+  window.open("https://renggarizky.github.io/istoria-website-sejarah/");
+});
+
+project3.addEventListener("click", () => {
+  window.open("https://renggarizky.github.io/bookshelf-apps/");
+});
